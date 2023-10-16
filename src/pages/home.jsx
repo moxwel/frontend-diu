a5ac170454c95ea17f45182a4308702a78529547
Felipe <felipe.melladoo@usm.cl>
2023-10-16 12:01:59 -0300

    Lista Eventos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import NavBar from '../components/nav_bar'
+import MyCalendar from '../components/calendar' 
+import ListaEventos from '../components/lista_eventos'
 
 export const HomePage = () => {
   return (
     <div className='page'>
       <h1 className='page__title'>Frontend Sample App</h1>
       <NavBar />
+      <MyCalendar />
+      <ListaEventos />
       <h2>Página de Inicio</h2>
     </div>
   )
