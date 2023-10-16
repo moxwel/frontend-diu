f990762c587a7ea117064abff3b67e0a1ed14dfb
CrissFLl <cfuentesll10@gmail.com>
2023-10-16 14:33:37 -0300

    compartir

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 import NavBar from '../components/nav_bar'
 import MyCalendar from '../components/calendar' 
 import ListaEventos from '../components/lista_eventos'
+import { Compartir } from '../components/compartir_evento' // Cambio aquí
+
+
 
 export const HomePage = () => {
+  const eventUrl = "localhost:5050"; // la url del evento
   return (
     <div className='page'>
       <h1 className='page__title'>Frontend Sample App</h1>
       <NavBar />
       <MyCalendar />
       <ListaEventos />
+      <Compartir url={eventUrl} /> // Cambio aquí
       <h2>Página de Inicio</h2>
     </div>
   )
