3612a58acc9261c175ba80179f34b32bec0d9a7c
moxwel <49621788+moxwel@users.noreply.github.com>
2023-10-19 14:24:41 -0300

    Boton eliminar

--- src/pages/evento.jsx
+++ src/pages/evento.jsx
@@ -1,8 +1,12 @@
 // Este es el componente Evento que muestra la página de un evento con su información
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { Compartir } from "../components/compartir_evento";
 import PageHeader from "../components/page_header";
+import { Grid } from "@mui/material";
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Evento({ eventos }) {
   // Obtiene el índice del evento desde el parámetro de la ruta
@@ -10,22 +14,44 @@ function Evento({ eventos }) {
   // Obtiene el evento desde la lista de eventos
   const evento = eventos[index];
 
+  const navigate = useNavigate();
+
+  const eliminarEvento = () => {
+    // Elimina el evento de la lista
+    eventos.splice(index, 1);
+    // Vuelve a la ruta /
+    navigate("/");
+  };
+
   return (
     <div className="page">
       <PageHeader />
-      <div className="Evento">
-        <h2>{evento.nombre}</h2>
-        <p>
-          Fecha: {evento.fechaInicio.slice(0, 10)} - {evento.fechaTermino.slice(0, 10)}
-        </p>
-        <p>
-          Hora: {evento.horaInicio} - {evento.horaTermino}
-        </p>
-        <p>Modalidad: {evento.modalidad}</p>
-        <p>Ubicación: {evento.modalidad === "online" ? <a href={evento.ubicacion}>{evento.ubicacion}</a> : evento.ubicacion}</p>
-        <p>Descripción: {evento.descripcion}</p>
-        <Compartir />
-      </div>
+
+      <Grid container direction="row">
+        <Grid item xs={6}>
+          <div className="Evento">
+            <h2>{evento.nombre}</h2>
+            <p>
+              Fecha: {evento.fechaInicio.slice(0, 10)} - {evento.fechaTermino.slice(0, 10)}
+            </p>
+            <p>
+              Hora: {evento.horaInicio} - {evento.horaTermino}
+            </p>
+            <p>Modalidad: {evento.modalidad}</p>
+            <p>Ubicación: {evento.modalidad === "online" ? <a href={evento.ubicacion}>{evento.ubicacion}</a> : evento.ubicacion}</p>
+            <p>Descripción: {evento.descripcion}</p>
+            <Compartir />
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          <h2>Administrar</h2>
+
+          {/* Cuando borra elementos, no los actualiza en el localstore, hay que arreglar eso */}
+          <Button variant="contained" color="error" onClick={eliminarEvento} startIcon={<DeleteIcon />}>
+            Eliminar evento
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/pages/internal.jsx
+++ src/pages/internal.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-import NavBar from "../components/nav_bar";
+import PageHeader from "../components/page_header";
 import LogoUSM from "../assets/usm.png";
 
 export const InternalPage = () => {
   return (
     <div className="page">
-      <h1 className="page__title">Frontend Sample App</h1>
-      <NavBar />
-      <h2>Página Interna</h2>
-      <img src={LogoUSM} height={100} alt="logo usm" />
-      <br />
-      <Button variant="contained">Material UI Button</Button>
+      <PageHeader />
+
+      
     </div>
   );
 };
